73fbb499cb2593e5c02c5cc192ea0d54e1a70115
Dan Uhl <duhl@wayfair.com>
2023-03-27 15:59:33 -0400

    remove extra console logs

--- scripts/deprecated/linkFixerIntegration.mjs
+++ scripts/deprecated/linkFixerIntegration.mjs
@@ -4,31 +4,16 @@ const replacerFnHTML = (match, p1, p2, p3, p4) => {
   console.log(`replacing ${match} with  ${p1}${p3}${p4}`);
   return `${p1}${p3}${p4}`;
 };
-// const replacerFn = (match, p1, p2, p3) => {
-//   console.log(`replacing ${match} with  ${p1}(${p3})`);
-//   return `${p1}(${p3})`;
-// };
 const fileRegex = /\.md/;
 
 const linkFixerVitePlugin = () => {
   return {
     name: "link fixer vite transform",
-    renderChunk(code, chunk, options, meta) {
-      // console.log({ code });
-    },
+    renderChunk(code, chunk, options, meta) { },
     transform(src, id) {
       // only act on markdown files
       if (fileRegex.test(id)) {
         const newCode = src.replaceAll(linkRegexHTML, replacerFnHTML);
-        if (src.match(linkRegexHTML) != null) {
-          console.log(id);
-          console.log("**************************************");
-          console.log(newCode);
-          console.log("**************************************");
-        }
-
-        // const newerCode = src.replaceAll("a", "z");
-        // console.log(`checking file ${id}`);
         return { code: newCode };
       } else {
         // console.log("no replacements for file: " + id);
