1dea7c8cb5383aa63e2d424be68a0d3ffc4a0ac6
Dan Uhl <duhl@wayfair.com>
2023-03-29 19:20:09 -0400

    removes date from file names

---
+++ scripts/quickRename.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const oldFileRegex = /^\d\d-\d\d-/;
+const ROOT = '/Users/danieluhl/git/astro-blog/src/content/blog';
+
+function main() {
+  const files = fs.readdirSync('/Users/danieluhl/git/astro-blog/src/content/blog');
+  files.forEach(file => {
+    if (oldFileRegex.test(file)) {
+      console.log(file);
+      console.log(file.replace(oldFileRegex, ''));
+      fs.renameSync(path.join(ROOT, file), path.join(ROOT, file.replace(oldFileRegex, '')));
+    }
+  })
+}
+
+main();
